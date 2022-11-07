b22112d2c6f7801d717821cd8da5199c502c9fd5
Jeon Min <emfp86@gmail.com>
2022-11-07 17:19:39 +0900

    마이페이지 수정완료 모달 추가
    마이페이지 수정완료 모달 추가 (/product/list)

---
+++ src/main/java/com/toodari/beansbox/controller/InputController.java
@@ -0,0 +1,13 @@
+package com.toodari.beansbox.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/input")
+@Log4j2
+@RequiredArgsConstructor
+public class InputController {
+}
