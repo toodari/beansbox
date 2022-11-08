16d389f5e7d70f0f5a26e446153893b344c5d519
Jeon Min <emfp86@gmail.com>
2022-11-08 12:46:43 +0900

    input list/register

--- src/main/java/com/toodari/beansbox/controller/InputController.java
+++ src/main/java/com/toodari/beansbox/controller/InputController.java
@@ -11,7 +11,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 
 @Controller
@@ -23,22 +29,43 @@ public class InputController {
 
     private final InputService service;
 
+    @GetMapping("/")
+    public String index(){
+        return "redirect:/input/list";
+    }
+
     @GetMapping("/list")
     public void list(PageRequestDTO pageRequestDTO, Model model){
 
         log.info("list.............." + pageRequestDTO);
 
         model.addAttribute("result", service.getList(pageRequestDTO));
+
     }
 
-    @PostMapping("/list")
+//    @PostMapping("/list")
+//    public String listPost(RedirectAttributes redirectAttributes) {
+//        redirectAttributes.addFlashAttribute("msg", "success");
+//
+//        return "redirect:/input/register";
+//    }
+
+//    @RequestMapping("/list")
+//    public Map<String, Object> insert(@RequestParam("chkArr[]") List<String> chkArr) throws Exception {
+//        Iterator<String> it =  chkArr.iterator();
+//
+//        //DB <- Genre processing
+//        //controller processing
+//    }
 
 
     @GetMapping("/register")
-    public void register(){
+    public void register(@RequestParam List<String> pnum, Model model){
 
+        log.info(pnum);
         log.info("register..............");
 
+        model.addAttribute("checked", pnum);
     }
 
     @PostMapping("/register")
--- src/main/java/com/toodari/beansbox/service/InputServiceImpl.java
+++ src/main/java/com/toodari/beansbox/service/InputServiceImpl.java
@@ -6,12 +6,14 @@ import com.toodari.beansbox.dto.ProductDTO;
 import com.toodari.beansbox.entity.Product;
 import com.toodari.beansbox.entity.ProductImage;
 import com.toodari.beansbox.repository.InputRepository;
+import com.toodari.beansbox.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
@@ -22,6 +24,7 @@ import java.util.function.Function;
 public class InputServiceImpl implements InputService{
 
     private final InputRepository inputRepository;
+    private final ProductRepository productRepository;
 
     @Override
     public PageResultDTO<ProductDTO, Object[]> getList(PageRequestDTO requestDTO){
@@ -39,6 +42,22 @@ public class InputServiceImpl implements InputService{
                 requestDTO.getPageable(Sort.by("pnum").descending()));
 
         return new PageResultDTO<>(result, fn);
+    }
+
+    @Override
+    public ProductDTO read(Long pnum) {
+        List<Object[]> result = productRepository.getProductWithImage(pnum);
+
+        Product product = (Product) result.get(0)[0];
+
+        List<ProductImage> productImageList = new ArrayList<>();
+
+        result.forEach(arr -> {
+            ProductImage productImage = (ProductImage) arr[1];
+            productImageList.add(productImage);
+        });
+
+        return entitiesToDTO(product, productImageList);
     }
 
     @Override
